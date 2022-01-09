f16f90dcba102d059eea1cba60a842e2c202b99d
davide-info <83416046+davide-info@users.noreply.github.com>
2022-01-09 19:15:24 +0100

    Aggiunto file

---
+++ prova/appClientModule/ProvaMainBoot.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan({"controllers"})
+public class ProvaMainBoot extends SpringBootServletInitializer{
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(ProvaMainBoot.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(ProvaMainBoot.class);
+		application.run(args);
+	}
+}
